8f20722e3b0e8c64059fe1621130c35115e93d0c
tcp72 <trevor.c.pettit@gmail.com>
2023-02-16 18:17:34 -0700

    Add project files.

---
+++ Block.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RootCoinTrial1
+{
+    class Block
+    {
+        public int Index { get; set; } //make a block
+        public string PreviousHash { get; set; }
+        public string TimeStamp { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "") //constructor
+        {
+            this.Index = index;
+            this.TimeStamp = timestamp;
+            this.Transactions = transactions;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.TimeStamp + this.Transactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", ""); //convert back to hex string
+        }
+        public void Mine(int difficulty) //vary hash until has is what we want; depending on number of 0
+        {
+            while (this.Hash.Substring(0, difficulty) != new String('0', difficulty)) //check from 0 to specified value
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                //Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("Block has mined: " + this.Hash);
+        }
+    }
+}
---
+++ Blockchain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RootCoinTrial1
+{
+    class Blockchain
+    {
+        public List<Block> Chain { get; set; }
+        public int Difficulty { get; set; }
+        List<Transaction> pendingTransactions { get; set; }
+        public decimal MiningReward { get; set; }
+        public Blockchain(int difficulty, decimal miningReward) //constructor
+        {
+            this.Chain = new List<Block>();
+            this.Chain.Add(CreateGenesisBlock());
+            this.Difficulty = difficulty;
+            this.MiningReward = miningReward;
+            this.pendingTransactions = new List<Transaction>();
+        }
+        public Block CreateGenesisBlock() //need genesis block that is special and not link to other blocks
+        {
+            return new Block(0, DateTime.Now.ToString("yyyyMMddHHmmssffff"), new List<Transaction>());
+        }
+
+        public Block GetLatestBlock() //method to get most recent
+        {
+            return this.Chain.Last();
+        }
+        public void AddBlock(Block newBlock)
+        {   //get hash of latest, add it to the previous hash property, make new hash, add both together
+            newBlock.PreviousHash = this.GetLatestBlock().Hash; //whatever was there become previous hash property of the one about to add
+            newBlock.Hash = newBlock.CalculateHash();
+            this.Chain.Add(newBlock);
+        }
+
+        public void addPendingTransaction (Transaction transaction)
+        {
+            if (transaction.FromAddress is null || transaction.ToAddress is null)
+            {
+                throw new Exception("Transactions must incude a to and from address.");
+            }
+            if(transaction.Amount > this.GetBalanceOfWallet(transaction.FromAddress))
+            {
+                throw new Exception("There must be sufficient money in the wallet!");
+            }
+            if (transaction.IsValid() == false)
+            {
+                throw new Exception("Cannot add an invalid transaction to a block.");
+            }
+            this.pendingTransactions.Add(transaction);
+        }
+
+        public decimal GetBalanceOfWallet(PublicKey address)
+        {
+            decimal balance = 0;
+
+            string addressDER = BitConverter.ToString(address.toDer()).Replace("-", ""); 
+
+            foreach (Block block in this.Chain)
+            {
+                foreach(Transaction transaction in block.Transactions)
+                {
+                    if (!(transaction.FromAddress is null))
+                    { 
+                        string fromDER = BitConverter.ToString(transaction.FromAddress.toDer()).Replace("-", "");
+                        if (fromDER == addressDER)
+                        {
+                            balance -= transaction.Amount; //when pay someone, comes out of your wallet
+                        }
+                    }
+
+                    string toDER = BitConverter.ToString(transaction.ToAddress.toDer()).Replace("-", "");
+                    if (toDER == addressDER)
+                    {
+                        balance += transaction.Amount;
+                    }
+
+                }
+            }
+            return balance;
+        }
+
+        public void MinePendingTransactions(PublicKey miningRewardWallet)
+        {
+            Transaction rewardTx = new Transaction(null, miningRewardWallet, MiningReward);
+            this.pendingTransactions.Add(rewardTx);
+
+            Block newBlock = new Block(GetLatestBlock().Index + 1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), this.pendingTransactions, GetLatestBlock().Hash);
+            newBlock.Mine(this.Difficulty);
+
+            Console.WriteLine("Block successfully mined!");
+            this.Chain.Add(newBlock);
+            this.pendingTransactions = new List<Transaction>();
+        }
+
+        public bool IsChainValid()
+        {
+            for (int i = 1; i < this.Chain.Count; i++)
+            {
+                Block currentBlock = this.Chain[i];
+                Block previousBlock = this.Chain[i - 1];
+                //check if current block hash is same as calculated hash
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
